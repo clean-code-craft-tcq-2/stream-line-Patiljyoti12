cff4c4200452d016b4fd0af2eb8d6259688923f2
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-04-05 11:01:48 +0530

    Update sender_test.py

--- sender_test.py
+++ sender_test.py
@@ -1,1 +1,14 @@
+import sender
+import unittest 
 
+class sender_test(unittest.TestCase):
+  def test_get_temperature_readings(self):
+    self.assertTrue((sender.get_temperature_readings(20,100,50))==True)
+   
+  def test_get_charge_rate_readings(self):
+    self.assertTrue((sender.get_charge_rate_readings(10,100,50))==True)
+
+    
+    
+if __name__ == '__main__':
+  unittest.main()
