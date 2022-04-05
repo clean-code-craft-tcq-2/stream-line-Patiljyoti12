e33321fa588d920aae634ad51ee1daaa079086c8
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-04-05 21:15:43 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -15,3 +15,5 @@ def get_charge_rate_readings(min_value,max_value,number):
     else:
        return False
 
+def print_to_console(result):
+ print("The samples list containg  the streamed data is {}".format(result))
