eab97486d46d4a1d1388ad910a1e05499c2e1444
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-04-05 11:53:14 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -9,7 +9,7 @@ def get_charge_rate_readings(min_value,max_value,number):
     result=readings_status(min_value,max_value,number)
     return result
 
-  def readings_status(min_value,max_value,number):
+def readings_status(min_value,max_value,number):
     if min_value>0 and max_value>0:
         result=random.sample(range(min_value, max_value), number)
         return True
