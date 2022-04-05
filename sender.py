8f6c7ec73e7584262d72b799b9bb9b3caa360e45
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-04-05 15:03:11 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -2,17 +2,16 @@
 import random
  
 def get_temperature_readings(min_value,max_value,number):
-    result=readings_status(min_value,max_value,number)
-    return result
+    result=random.sample(range(min_value, max_value), number)
+    if result!=[]:
+       return True
+    else:
+       return False
     
 def get_charge_rate_readings(min_value,max_value,number):
-    result=readings_status(min_value,max_value,number)
-    return result
-
-def readings_status(min_value,max_value,number):
-    if min_value>0 and max_value>0:
-        result=random.sample(range(min_value, max_value), number)
-        return True
+    result=random.sample(range(min_value, max_value), number)
+    if result!=[]:
+       return True
     else:
-        return False
-    
+       return False
+
