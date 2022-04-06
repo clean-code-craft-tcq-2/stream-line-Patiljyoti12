b4d1c6fbc2e2550076342c6fa46a22a8ee305b2d
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-04-06 16:58:54 +0530

    Update sender_test1.py

--- sender_test1.py
+++ sender_test1.py
@@ -7,7 +7,7 @@ def test_generate_samples(self):
 def test_temperature_readings(self):
     self.assertTrue(sender1.get_temperature_readings(20,100,50)!=[])
     
-def test_celcius_to_Farenheit_converter():
+def test_celcius_to_Farenheit_converter(self):
     self.assertTrue(sender1.celcius_to_Farenheit_convertor([10,20,30,40]==[50,68,86,104]))
     
 def test_chjarge_rate_readings(self):
