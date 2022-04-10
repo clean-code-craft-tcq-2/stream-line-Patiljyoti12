0630c537948b06f211dd3c0539dfe6581d4279b0
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-10 10:29:07 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -7,7 +7,7 @@ def getDatafromConsoleOutput():
     for character in line:
       if character.find("temperature"):
         print(character)
-      else if character.find("charge_rate"):
+      elseif character.find("charge_rate"):
         print(character)
         
 if __name__ == '__main__':
