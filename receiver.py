ffb14770a3ca0c33ae8561aff291fc9cfed9174d
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-10 15:29:00 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -6,11 +6,6 @@ def getDatafromConsoleOutput():
     temperature =[]
     line=line.split('[')
     print(line)
-    index=0
-    if "temperature" in line[index]:
-      while("charge_rate" is not in line[index]):
-        index=index+1
-        tempeataure = temperature.append(line[index])
 
 if __name__ == '__main__':
   getDatafromConsoleOutput()
