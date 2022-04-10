316505736b36bb30184a0ff42a044f1f5af7cb6d
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-10 15:18:19 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -4,7 +4,8 @@ import sys
 def getDatafromConsoleOutput():
   for line in sys.stdin:
     line=line.split('[')
-    print(line)
+    if "temperature" in line:
+      print(line)
 
 if __name__ == '__main__':
   getDatafromConsoleOutput()
