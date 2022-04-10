51ce9246a06a963fcab9d661293fc1ab1fce2341
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-10 10:32:31 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -5,6 +5,7 @@ def getDatafromConsoleOutput():
   for line in sys.stdin:
     line=line.split(',')
     for character in line:
+      print(character)
       if character.find("temperature"):
         print(character)
       elif character.find("charge_rate"):
