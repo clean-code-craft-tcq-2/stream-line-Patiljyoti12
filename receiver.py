9512f46aa6bf14cc982c0d5f67f50b52de263689
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-10 15:43:39 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -4,12 +4,15 @@ import sys
 def getDatafromConsoleOutput():
   for line in sys.stdin:
     temperature = []
+    charge_rate = []
     line=line.split(',')
     index=0
     if "temperature" in line[index]:
       while(not("charge_rate" in line[index])):
         temperature.append(line[index])
         index = index+1
+      for i in range(index-1,len(line)):
+        charge_rate = charge_rate.append(line[i])
     print(temperature)
       
 
