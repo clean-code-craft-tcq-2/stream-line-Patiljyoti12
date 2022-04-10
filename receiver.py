79857a6007ccd593eb3c79fd56ad54aaf6e69981
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-10 19:05:13 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -41,6 +41,8 @@ def getDatafromConsoleOutput():
       print(temperature1[i])
     maximum_temperature = getMaximumtemperature(temperature1)
     minimum_temperature = getMinimumtemperature(temperature1)
+    print(maximum_temperature)
+    print(minimum_temperature)
             
 if __name__ == '__main__':
   getDatafromConsoleOutput()
