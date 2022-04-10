bcc4fd4c42b788cf476f348ace9697208ff41beb
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-10 16:00:04 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -17,7 +17,8 @@ def getDatafromConsoleOutput():
     print(temperature)
     print(charge_rate)
     for i in range(len(temperature)):
-      temperature[i] = re.findall(str(temperature[i]))
+      temperature[i] = str(temperature[i])
+      temperature[i] = re.findall(temperature[i])
     print(temperature)
       
 
