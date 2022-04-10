69c6200a5d222e65deac8c59bb67579678b64e52
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-10 19:40:23 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -4,17 +4,17 @@ import re
 
 def getMinimumtemperature(temperature):
   minimum_temperature = temperature[0]
-  for i in range(len(temperature)):
-    if minimum_temperature>temperature[i]:
-      minimum_temperature = temperature[i]
+  for index in range(len(temperature)):
+    if minimum_temperature>temperature[index]:
+      minimum_temperature = temperature[index]
   return minimum_temperature
 
 
 def getMaximumtemperature(temperature):
   maximum_temperature = temperature[0]
-  for i in range(len(temperature)):
-    if maximum_temperature<temperature[i]:
-      maximum_temperature = temperature[i]
+  for index in range(len(temperature)):
+    if maximum_temperature<temperature[index]:
+      maximum_temperature = temperature[index]
   return maximum_temperature
 
 def getMinimumChargeRate(charge_rate):
@@ -24,15 +24,25 @@ def getMaximumChargeRate(charge_rate):
   return max(charge_rate)
 
 def appendNewListWithFloatValue(batteryParameterOld,batteryParameterNew):
-  for i in range(len(batteryParameterOld)):
-    batteryParameterNew.append(float(batteryParameterOld[i][0]))
+  for index in range(len(batteryParameterOld)):
+    batteryParameterNew.append(float(batteryParameterOld[index][0]))
   return batteryParameterNew
 
 def filterBatteryParameterValue(batteryParameter):
   for index in range(len(batteryParameter)):
     batteryParameter[index]=re.findall(r"[-+]?\d*\.\d+|\d+", batteryParameter[index])
   return batteryParameter
-      
+
+def segregateTemperatureandChargerate(line,temperature,charge_rate):
+  index=0
+  if "temperature" in line[index]:
+    while(not("charge_rate" in line[index])):
+      temperature.append(line[index])
+      index = index+1
+    for i in range(index,len(line)):
+      charge_rate.append(line[i])
+  return temperature,charge_Rate
+
 def getDatafromConsoleOutput():
   for line in sys.stdin:
     temperature = []
@@ -40,13 +50,7 @@ def getDatafromConsoleOutput():
     charge_rate = []
     charge_rate1 = []
     line=line.split(',')
-    index=0
-    if "temperature" in line[index]:
-      while(not("charge_rate" in line[index])):
-        temperature.append(line[index])
-        index = index+1
-      for i in range(index,len(line)):
-        charge_rate.append(line[i])
+    temperature,charge_rate = segregateTemperatureandChargerate(line,temperature,charge_rate)
     temperature=filterBatteryParameterValue(temperature)
     charge_rate=filterBatteryParameterValue(charge_rate)
     temperature1 = appendNewListWithFloatValue(temperature,temperature1)
