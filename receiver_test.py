3decae44729613d0bd89397eba2f195a519e9a5b
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-20 08:04:36 +0530

    Update receiver_test.py

--- receiver_test.py
+++ receiver_test.py
@@ -5,14 +5,11 @@ class receiver_test(unittest.TestCase):
   def test_getMinimumtemperature(self):
     self.assertTrue(receiver.getMinimumtemperature([25,30,21,50,60])==21)
 
-
   def test_getMaximumtemperature(self):
     self.assertTrue(receiver.getMaximumtemperature([25,30,21,50,60])==60)
 
-
   def test_getMinimumChargeRate(self):
     self.assertTrue(receiver.getMinimumChargeRate([40,30,55,50,60])==30)
-
 
   def test_getMaximumtemperature(self):
     self.assertTrue(receiver.getMaximumChargeRate([40,30,55,50,60])==60)
@@ -20,5 +17,10 @@ class receiver_test(unittest.TestCase):
   def test_filterBatteryParameterValue(self):
     self.assertTrue(receiver.filterBatteryParameterValue(['*12.1/','22.0/','*25.8*','*8.09*','60.0'])==[['12.1'],['22.0'],['25.8'],['8.09'],['60.0']])
 
+  def test_appendNewListWithFloatValue(self):
+    newList =[]
+    receiver.appendNewListWithFloatValue([['12.1'],['22.0'],['25.8'],['8.09'],['60.0']],newList)
+    self.assertTrue(newList == [22.1,22.0,25.8,8.09,60.0])
+    
 if __name__ == '__main__':
   unittest.main()
