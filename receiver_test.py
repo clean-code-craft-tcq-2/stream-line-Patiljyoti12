d22ddde34e8b20862f0f993cc2e2b2d3de87a9aa
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-20 08:08:45 +0530

    Update receiver_test.py

--- receiver_test.py
+++ receiver_test.py
@@ -21,7 +21,7 @@ class receiver_test(unittest.TestCase):
     newList =[]
     receiver.appendNewListWithFloatValue([['12.1'],['22.0'],['25.8'],['8.09'],['60.0']],newList)
     print(newList)
-    self.assertTrue(newList == [22.1,22.0,25.8,8.09,60.0])
+    self.assertTrue(newList == [12.1,22.0,25.8,8.09,60.0])
     
 if __name__ == '__main__':
   unittest.main()
