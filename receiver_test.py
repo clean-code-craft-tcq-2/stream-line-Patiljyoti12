aacb8d06aaa04a6f44be2f47b6acd3239f07b784
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-04-20 07:33:52 +0530

    Update receiver_test.py

--- receiver_test.py
+++ receiver_test.py
@@ -1,20 +1,21 @@
 from receiver import *
 import unittest
 
-def test_getMinimumtemperature(self):
-  self.assertEqual(receiver.getMinimumtemperature(25,30,21,50,60)==21)
+class receiver_test(unittest.TestCase):
+  def test_getMinimumtemperature(self):
+    self.assertEqual(receiver.getMinimumtemperature(25,30,21,50,60)==21)
 
 
-def test_getMaximumtemperature(self):
-  self.assertEqual(receiver.getMaximumtemperature(25,30,21,50,60)==60)
+  def test_getMaximumtemperature(self):
+    self.assertEqual(receiver.getMaximumtemperature(25,30,21,50,60)==60)
 
 
-def test_getMinimumChargeRate(self):
-  self.assertEqual(receiver.getMinimumChargeRate(40,30,55,50,60)==30)
+  def test_getMinimumChargeRate(self):
+    self.assertEqual(receiver.getMinimumChargeRate(40,30,55,50,60)==30)
 
 
-def test_getMaximumtemperature(self):
-  self.assertEqual(receiver.getMaximumChargeRate(40,30,55,50,60)==55)
+  def test_getMaximumtemperature(self):
+    self.assertEqual(receiver.getMaximumChargeRate(40,30,55,50,60)==55)
 
 if __name__ == '__main__':
   unittest.main()
