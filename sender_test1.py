98aba99e11e3682dee2420bbec18a46c7a11565a
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-04-20 22:52:10 +0530

    Update sender_test1.py

--- sender_test1.py
+++ sender_test1.py
@@ -1,19 +1,27 @@
-from sender1 import *
-import unittest
+import sender1
 
-def test_generate_samples(self):
-    self.assertEqual(len(sender1.generate_samples(10,60,50)==50))
+def test_celcius_to_Farenheit_convertor(temperature_samples,expected_temperature_samples):
+    fah_temperature_samples=sender1.celcius_to_Farenheit_convertor(temperature_samples)
+    assert(fah_temperature_samples==expected_temperature_samples)
+
+
+def test_generate_samples(min_value,max_value,no_of_samples,expected_length_value):
+    result=sender1.generate_samples(min_value,max_value,no_of_samples)
+    resultantsamples_length=len(result)
+    assert(resultantsamples_length==expected_length_value)
     
-def test_temperature_readings(self):
-    self.assertTrue(sender1.get_temperature_readings(20,100,50)!=[])
+def test_charge_rate_readings(min_value,max_value,no_of_samples,expected_length_value):
+    charge_rate_samples=sender1.get_charge_rate_readings(min_value,max_value,no_of_samples)
+    resultantsamples_length=len(charge_rate_samples)
+    assert(resultantsamples_length==expected_length_value)
+ 
+
     
-def test_celcius_to_Farenheit_converter(self):
-    self.assertTrue(sender1.celcius_to_Farenheit_convertor([10,20,30,40]==[50,68,86,104]))
-    
-def test_chjarge_rate_readings(self):
-    self.assertEqual(len(sender1.get_charge_rate_readings(30,120,50)==50))
-   
+
 if __name__ == '__main__':
-  print(display_samples_on_console(20,80,50))
-  unittest.main()
+   test_celcius_to_Farenheit_convertor([10,20,30,40],[50.0,68.0,86.0,104.0])
+   test_generate_samples(20,100,50,50)
+   test_charge_rate_readings(20,40,10,10)
+  
+  
     
